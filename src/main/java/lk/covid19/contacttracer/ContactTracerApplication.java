53423bcdf1643941bf960bc7ed29ab357ca1999a
LalithK90 <asakahatapitiya@gmail.com>
2021-08-20 21:50:15 +0530

    initial commit

---
+++ src/main/java/lk/covid19/contacttracer/ContactTracerApplication.java
@@ -0,0 +1,13 @@
+package lk.covid19.contacttracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactTracerApplication.class, args);
+	}
+
+}
