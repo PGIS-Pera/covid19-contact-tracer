0017b21de522f8a0b2c283d61ec8426253505155
LalithK90 <asakahatapitiya@gmail.com>
2021-08-24 00:12:56 +0530

    application properties, docker config, js,css,excel, some config xml were updated

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    changePageAndSize();
+});
+
+changePageAndSize = () => {
+    $('#pageSizeSelect').change(evt => {
+        window.location.replace(`/patient/?pageSize=${evt.target.value}&page=1`);
+    });
+}
