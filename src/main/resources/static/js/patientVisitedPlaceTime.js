891135bb064d2c1a36e61d56ce04d7daebbed323
LalithK90 <asakahatapitiya@gmail.com>
2021-10-18 12:36:15 +0530

    location interact place search gramaniladhari show details was changed

--- src/main/resources/static/js/patientVisitedPlaceTime.js
+++ src/main/resources/static/js/patientVisitedPlaceTime.js
@@ -26,7 +26,11 @@ function gramaniladhari(val) {
                 let html_first_part = `<ul class="list-group list-group-flush justify-content-center" style="width: max-content">`;
                 let html_middle_part = ``;
                 for (let i = 0; i < data.length; i++) {
-                    html_middle_part += `<li id="${filedId}-${data[i].id}" onClick="selectGramaNiladhari(this)" class="list-group-item font-weight-bold p">${data[i].name} - ${data[i].number}</li >`;
+                    html_middle_part += `<li id="${filedId}-${data[i].id}" onClick="selectGramaNiladhari(this)" class="list-group-item font-weight-bold p">
+${data[i].name} - ${data[i].number} -  
+${data[i].dsOffice.name} - ${data[i].dsOffice.district.name} - ${data[i].dsOffice.district.province.province}
+
+</li >`;
                 }
                 // $("#gramaNiladhari-box");
                 $(`#${filedId}-box`).show().html(html_first_part + html_middle_part);
