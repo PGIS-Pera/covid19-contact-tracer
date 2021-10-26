1262b67aec34e0bf49ef50b24185e0b2e1ad628d
LalithK90 <asakahatapitiya@gmail.com>
2021-10-26 23:13:10 +0530

    test case was start

--- src/test/java/lk/covid19/contact_tracer/ContactTracerApplicationTests.java
+++ src/test/java/lk/covid19/contact_tracer/ContactTracerApplicationTests.java
@@ -1,8 +1,14 @@
 package lk.covid19.contact_tracer;
 
+import lk.covid19.contact_tracer.asset.person.entity.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableAutoConfiguration( exclude = {DataSourceAutoConfiguration.class} )
 @SpringBootTest
 class ContactTracerApplicationTests {
 
